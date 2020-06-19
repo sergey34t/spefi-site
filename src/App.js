c80e3f5cf02e0d3989eddf5367754eef4b4b72f4
sergey34t <sergey.tsimanov@gmail.com>
2020-06-19 07:35:51 +0300

    Add store and main component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import s from './App.module.css';
 const App = () => {
     return (
         <div className={s.container}>
+
+            {/*<div className={s.dropdownContent}>
+                <p className={s.dropItem + ' ' + s.visible}></p>
+                <p className={s.dropItem + ' ' + s.hidden}></p>
+                <p className={s.dropItem + ' ' + s.hidden}></p>
+            </div>*/}
+
+            <select className={s.dropdownContent}>
+                <option className={s.dropItem + ' ' + s.visible}  value="volvo">Volvo</option>
+                <option className={s.dropItem + ' ' + s.visible} value="saab">Saab</option>
+                <option className={s.dropItem + ' ' + s.visible}  value="mercedes">Mercedes</option>
+                <option className={s.dropItem + ' ' + s.visible}  value="audi">Audi</option>
+            </select>
+
             <input type="text" name="" className={s.inputTxt} placeholder="Enter domain...."/>
             <div className={s.inputBtn}></div>
         </div>
